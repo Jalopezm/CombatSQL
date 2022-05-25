8de385b28181ee45852578efe837c95f29534393
JuanAn López <jalopezm@esliceu.net>
2022-05-25 20:16:41 +0200

    Connection A la Base de Datos

--- src/Menu/MenuLogin.java
+++ src/Menu/MenuLogin.java
@@ -1,31 +1,33 @@
 package Menu;
 
-import domain.Usuario;
-
+import DAO.MySql_Implementation.UsuarioDaoMySql;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.sql.Connection;
 
-public class MenuRegistro extends Menu{
-    public MenuRegistro(String title) {
+import Conection.ClaseSingleton;
+
+public class MenuLogin extends Menu {
+    public MenuLogin(String title) {
         super(title);
     }
 
     @Override
     protected void initActions() {
+
     }
 
     @Override
     protected void onPreOptions() {
-        String nombre = Input.readString("Introduce tu Nombre");
-        String apellido = Input.readString("Introduce tu apellido");
+
+        Connection con = ClaseSingleton.getConnection();
+
         String nombreUsuario = Input.readString("Introduce el Nombre del Usuario");
-        String correo = Input.readString("Introduce un email");
         String contraseña = Input.readString("Introduce una Contraseña");
-
-        Usuario usuario = new Usuario(nombre,apellido,nombreUsuario,correo,contraseña);
-        System.out.println("Nuevo Usuario Creado: " + usuario.getNombre());
-
+        String contraseñaHash = "";
+        //Hash Contraseña
         try {
+
             MessageDigest md = MessageDigest.getInstance("SHA-512");
             String passwordToHash = "";
             byte[] bytes = md.digest(passwordToHash.getBytes());
@@ -34,12 +36,15 @@ public class MenuRegistro extends Menu{
                 sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16)
                         .substring(1));
             }
-            String generatedPassword = sb.toString();
+            contraseñaHash = sb.toString();
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
 
-        //        Si nombre de usuario existe error Creando usuario
+        UsuarioDaoMySql uDMySql = new UsuarioDaoMySql(con);
 
+        if (uDMySql.login(nombreUsuario, contraseñaHash)) {
+
+        }
     }
 }
