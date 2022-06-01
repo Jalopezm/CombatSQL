0283e812df39abc45a56149f24f051ff6d053d2e
JuanAn López <jalopezm@esliceu.net>
2022-06-01 19:42:02 +0200

    Menu Creacion Pj

---
+++ src/Menu/MenuCreacionPj.java
@@ -0,0 +1,17 @@
+package Menu;
+
+public class MenuCreacionPj extends Menu{
+    public MenuCreacionPj (String title) {
+        super(title);
+    }
+
+    @Override
+    protected void initActions() {
+
+    }
+
+    @Override
+    protected void onPreOptions() {
+
+    }
+}
