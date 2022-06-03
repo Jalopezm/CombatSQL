01f25d71f503b0e25ca58a64533f05bc5427c259
Eduardo González Lorenzo <edugonzalezlorenzo@gmail.com>
2022-06-03 19:57:38 +0200

    Add InventarioDaoMySql.java

---
+++ src/DAO/MySql_Implementation/InventarioDaoMySql.java
@@ -0,0 +1,64 @@
+package DAO.MySql_Implementation;
+
+import DAO.InventarioDao;
+import domain.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class InventarioDaoMySql implements InventarioDao {
+    private Connection con;
+
+    public InventarioDaoMySql(Connection con) {
+        this.con = con;
+    }
+
+    @Override
+    public List<Inventario> getPersonajeInventario(int personajeID) {
+        try {
+            //Preparación de la consulta
+            PreparedStatement getAllStmnt = con.prepareStatement("SELECT * FROM INVENTARIO WHERE personajeID = ?");
+
+            //Sustitución de los ?
+            getAllStmnt.setInt(1, personajeID);
+
+            //Ejecución y guardado de la info de la query
+            ResultSet result = getAllStmnt.executeQuery();
+            List<Inventario> inventario = new ArrayList<>();
+
+            while (result.next()) {
+                inventario.add(new Inventario(
+                        result.getInt("experiencia"),
+                        result.getInt("monedas"))
+                );
+            }
+            return inventario;
+
+        } catch (SQLException e) {
+            System.err.println(e);
+        }
+        return null;
+    }
+
+
+    @Override
+    public boolean addObjecto(Objeto objeto) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteObjeto(Inventario inventario) {
+        return false;
+    }
+
+    @Override
+    public boolean inventarioContainsObjeto(Objeto objeto) {
+        return false;
+    }
+
+
+}
