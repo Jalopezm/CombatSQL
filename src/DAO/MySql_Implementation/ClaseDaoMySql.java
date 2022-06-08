fad7f7155314d60779255227df079df6b32ae247
eduar <edugonzalezlorenzo@gmail.com>
2022-06-08 14:39:02 +0200

    Primer arreglo de Clase

--- src/DAO/MySql_Implementation/ClaseDaoMySql.java
+++ src/DAO/MySql_Implementation/ClaseDaoMySql.java
@@ -12,29 +12,49 @@ public class ClaseDaoMySql implements ClaseDao {
         this.con = con;
     }
 
+//    @Override
+//    public int[] getAtributes(String nombre) {
+//        try {
+//            //Preparación de la consulta
+//            PreparedStatement getAllStmnt = con.prepareStatement("SELECT * FROM CLASE WHERE nombreClase = ?");
+//
+//            //Sustitución de los ?
+//            getAllStmnt.setString(1, nombre);
+//
+//            //Ejecución y guardado de la info de la query
+//            ResultSet result = getAllStmnt.executeQuery();
+//
+//            if (result.next()) {
+//                return new int[]{
+//                        result.getInt("ataque"),
+//                        result.getInt("evasion"),
+//                        result.getInt("habilidad"),
+//                        result.getInt("salud")
+//                };
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+
     @Override
-    public int[] getAtributes(String nombre) {
-        try {
-            //Preparación de la consulta
-            PreparedStatement getAllStmnt = con.prepareStatement("SELECT * FROM CLASE WHERE nombreClase = ?");
+    public int getAtk(String nombreClase) {
+        return 0;
+    }
 
-            //Sustitución de los ?
-            getAllStmnt.setString(1, nombre);
+    @Override
+    public int getEv(String nombreClase) {
+        return 0;
+    }
 
-            //Ejecución y guardado de la info de la query
-            ResultSet result = getAllStmnt.executeQuery();
+    @Override
+    public int getHab(String nombreClase) {
+        return 0;
+    }
 
-            if (result.next()) {
-                return new int[]{
-                        result.getInt("ataque"),
-                        result.getInt("evasion"),
-                        result.getInt("habilidad"),
-                        result.getInt("salud")
-                };
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
+    @Override
+    public int getVidaMax(String nombreClase) {
+        return 0;
     }
 }
